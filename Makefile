c2d5abd2235c0bac5876d7f6f6bc2362932d4f3a
Mathius <info@yogimathius.dev>
2024-01-27 11:06:13 -0800

    chore: add inst_live
    Co-authored-by: Evgeniia <Zhenia-Magic@users.noreply.github.com>

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 buildTests : createBuildDir
-	gcc -o tests/build/test tests/cmocka_test.c -l cmocka -L /usr/local/lib
+	gcc -o tests/build/test tests/inst_live_test.c src/operations/inst_live.c  -l cmocka -L /usr/local/lib
 
 createBuildDir :
 	mkdir -p build
---
+++ tests/inst_live_test.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "../include/op.h"
+
+static void true_test() {
+	assert_true(inst_live() == 1);
+}
+
+int main(void) {
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(true_test),
+	};
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
