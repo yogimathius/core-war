3499151f8a278054fa06d04d98a8f6ece05cfac3
Mathius <info@yogimathius.dev>
2024-01-28 00:56:38 -0800

    chore: add tests for read and write to virtual machine

--- tests/vm_test.c
+++ tests/vm_test.c
@@ -19,19 +19,21 @@ static void test_initialize_memory(void **state) {
 }
 
 static void test_read_memory(void **state) {
-
+    virtualMachine.registers[0] = 42;  // Load register 0 with 42
+    assert_true(readVirtualMachine(&virtualMachine, 0) == 42);
 }
 
 static void test_write_memory(void **state) {
-
+    writeVirtualMachine(&virtualMachine, 1, 24);
+    assert_true(readVirtualMachine(&virtualMachine, 1) == 24);
 }
 
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(test_initialize_memory),
-        // cmocka_unit_test(test_read_memory),
-        // cmocka_unit_test(test_write_memory),
+        cmocka_unit_test(test_read_memory),
+        cmocka_unit_test(test_write_memory),
         // Add more test cases as needed
     };
 
