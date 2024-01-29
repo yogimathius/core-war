2632215b6ff91085fd54fe360d6c5c534c18eff7
Mathius <info@yogimathius.dev>
2024-01-29 01:03:13 -0800

    chore: move assembly defs to assembly.h

--- include/op.h
+++ include/op.h
@@ -9,27 +9,6 @@
 // Maximum number of arguments an operation can take
 #define MAX_ARGS_NUMBER 4
 
-// Character denoting comments in the code
-#define COMMENT_CHAR '#'
-
-// Character denoting labels in the assembly code
-#define LABEL_CHAR ':'
-
-// Character denoting direct addressing mode in assembly code
-#define DIRECT_CHAR '%'
-
-// Character used to separate arguments in assembly code
-#define SEPARATOR_CHAR ','
-
-// Characters allowed in labels
-#define LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
-
-// Directive string for the program name
-#define NAME_CMD_STRING ".name"
-
-// Directive string for the comment
-#define COMMENT_CMD_STRING ".comment"
-
 // Argument types
 typedef char args_type_t;
 
