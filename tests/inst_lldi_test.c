344f64b4a088998076b1d5fb6007632eebc7a26e
Mathius <info@yogimathius.dev>
2024-01-28 23:55:05 -0800

    chore: add basic tests for inst_lldi

---
+++ tests/inst_lldi_test.c
@@ -0,0 +1,10 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "../include/op.h"
+#include "../include/tests.h"
+
+void inst_lldi_test() {
+	assert_true(inst_lldi() == 0);
+}
