735fc99fa098066c58bb34f5932e3cc28bd9e4bb
Mathius <info@yogimathius.dev>
2024-01-28 23:56:02 -0800

    chore: add basic tests for inst_xor

---
+++ tests/inst_xor_test.c
@@ -0,0 +1,10 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "../include/op.h"
+#include "../include/tests.h"
+
+void inst_xor_test() {
+	assert_true(inst_xor() == 0);
+}
