195b9fdf672414e47060c0985b7e002402336491
Mathius <info@yogimathius.dev>
2024-01-28 23:56:52 -0800

    chore: add instruction tests to tests.h

--- include/tests.h
+++ include/tests.h
@@ -11,4 +11,19 @@ void true_test();
 void passing_test();
 
 void inst_live_test();
+void inst_ld_test();
+void inst_st_test();
+void inst_add_test();
+void inst_sub_test();
+void inst_and_test();
+void inst_or_test();
+void inst_xor_test();
+void inst_zjmp_test();
+void inst_ldi_test();
+void inst_sti_test();
+void inst_fork_test();
+void inst_lld_test();
+void inst_lldi_test();
+void inst_lfork_test();
+void inst_aff_test();
 #endif
