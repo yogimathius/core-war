ceb7bd33718b205e80c9949bd504fa7662bbf87a
Mathius <info@yogimathius.dev>
2024-01-28 23:56:08 -0800

    chore: add basic tests for inst_zjmp

---
+++ tests/inst_zjmp_test.c
@@ -0,0 +1,10 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "../include/op.h"
+#include "../include/tests.h"
+
+void inst_zjmp_test() {
+	assert_true(inst_live() == 0);
+}
