13d916788ef1aa5292f8817f35e523556645e890
Mathius <info@yogimathius.dev>
2024-01-29 01:03:13 -0800

    chore: move assembly defs to assembly.h

---
+++ include/assembly.h
@@ -0,0 +1,25 @@
+#ifndef _CHAMPION_H_
+#define _CHAMPION_H_
+
+// Character denoting comments in the code
+#define COMMENT_CHAR '#'
+
+// Character denoting labels in the assembly code
+#define LABEL_CHAR ':'
+
+// Character denoting direct addressing mode in assembly code
+#define DIRECT_CHAR '%'
+
+// Character used to separate arguments in assembly code
+#define SEPARATOR_CHAR ','
+
+// Characters allowed in labels
+#define LABEL_CHARS "abcdefghijklmnopqrstuvwxyz_0123456789"
+
+// Directive string for the program name
+#define NAME_CMD_STRING ".name"
+
+// Directive string for the comment
+#define COMMENT_CMD_STRING ".comment"
+
+#endif
