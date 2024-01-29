cdb18de92818a17575ed88c2adf5d1a56ada8072
Mathius <info@yogimathius.dev>
2024-01-28 23:53:47 -0800

    chore: add basic tests for inst_aff

---
+++ tests/inst_aff_test.c
@@ -0,0 +1,10 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+#include "../include/op.h"
+#include "../include/tests.h"
+
+void inst_aff_test() {
+	assert_true(inst_aff() == 0);
+}
