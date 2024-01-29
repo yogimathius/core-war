cb6642f77b0a9f6935bf02d904289dd3bbc48d6e
Mathius <info@yogimathius.dev>
2024-01-28 23:20:49 -0800

    fix: remove static and main from cmocka_test.c

--- tests/cmocka_test.c
+++ tests/cmocka_test.c
@@ -2,20 +2,12 @@
 #include <stddef.h>
 #include <setjmp.h>
 #include <cmocka.h>
+#include "../include/tests.h"
 
-static void true_test() {
+void true_test() {
 	assert_true(1 != 2);
 }
 
-static void passing_test() {
+void passing_test() {
 	assert_false(2 != 2);
-}
-
-
-int main(void) {
-	const struct CMUnitTest tests[] = {
-		cmocka_unit_test(true_test),
-		cmocka_unit_test(passing_test),
-	};
-	return cmocka_run_group_tests(tests, NULL, NULL);
 }
