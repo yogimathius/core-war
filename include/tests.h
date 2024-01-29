5e40b6ce6fde423d699bfab219ce82212e7563f9
Mathius <info@yogimathius.dev>
2024-01-28 23:20:15 -0800

    chore: add tests.h

---
+++ include/tests.h
@@ -0,0 +1,14 @@
+#ifndef TESTS_H_
+#define TESTS_H_
+
+void test_initialize_memory();
+
+void test_read_memory();
+
+void test_write_memory();
+
+void true_test();
+void passing_test();
+
+void inst_live_test();
+#endif
