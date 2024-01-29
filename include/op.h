b4d1dff7e257e029e73c5763c67b1c92224d31ad
Mathius <info@yogimathius.dev>
2024-01-28 23:20:32 -0800

    chore: update op.h operation args

--- include/op.h
+++ include/op.h
@@ -52,7 +52,7 @@ typedef struct op_s {
   args_type_t type[MAX_ARGS_NUMBER];  // Types of arguments
   char code;            // Operation code
   int nbr_cycles;       // Number of cycles for the operation
-  int (*inst)(champion_t *, core_t *, code_t, int *);  // Function pointer for the instruction
+  int (*inst)();  // Function pointer for the instruction
 } op_t;
 
 // Enumeration of operation types
@@ -122,14 +122,14 @@ typedef struct header_s {
 Takes 1 parameter: 4 bytes that represent the player's number.
 Indicates the player is alive.
 */
-int inst_live(champion_t *, core_t *, code_t, int *);
+int inst_live();
 
 /* 
 Takes 2 parameters: loads the first parameter to the second parameter.
 Second parameter is a register. Ex: ld 34, r3 loads the REG_SIZE bytes
 starting from the Counter + 34 % IDX_MOD into r3.
 */
-int inst_ld(champion_t *, core_t *, code_t, int *);
+int inst_ld();
 
 /* 
 Takes 2 parameters: Stores first parameter (a register) into the second
@@ -137,42 +137,42 @@ parameter (it can be another register or a number). Ex: st r4, 34 stores
 r4 at the address of Counter + 34 % IDX_MOD; st r3, r8 copies r3 into
 r8.
 */
-int inst_st(champion_t *, core_t *, code_t, int *);
+int inst_st();
 
 /* 
 Takes 3 parameters: 3 registers. Add the first to the second, and store
 the result to the third. Modifies the carry.
 */
-int inst_add(champion_t *, core_t *, code_t, int *);
+int inst_add();
 
 /* 
 Same as add, but Subtracting. Modifies the carry.
 */
-int inst_sub(champion_t *, core_t *, code_t, int *);
+int inst_sub();
 
 /* 
 Same as add, and sub, but does a binary operation AND between the
 first and the second, and storing the result in the third parameter.
 Modifies the carry.
 */
-int inst_and(champion_t *, core_t *, code_t, int *);
+int inst_and();
 
 /* 
 Same as and, but performing an OR.
 */
-int inst_or(champion_t *, core_t *, code_t, int *);
+int inst_or();
 
 /* 
 Same as and and or, but performing an XOR.
 */
-int inst_xor(champion_t *, core_t *, code_t, int *);
+int inst_xor();
 
 /* 
 Takes 1 parameter, that must be an index. Jumps to this index if carry is
 1. Else, does nothing, but still consume time.
 Zjmp %23 -> if carry == 1, Counter + 23 % IDX_MOD to Counter
 */
-int inst_zjmp(champion_t *, core_t *, code_t, int *);
+int inst_zjmp();
 
 /* 
 Takes 3 parameters. First two are indexes and the third one is a register
@@ -180,42 +180,42 @@ Ex: ldi 3, %4, r1 -> reads IND_SIZ bytes from address Counter + 3 %
 IDX_MOD, add 4 to this value (SumResult). REG_SIZE byte are read
 from the Counter + SumResult % IDX_MOD and copies to r1.
 */
-int inst_ldi(champion_t *, core_t *, code_t, int *);
+int inst_ldi();
 
 /* 
 Takes 3 parameters. The first one is a register. The other two can either
 be indexes or registers. Ex: sti r2, %4, %5 -> copies the content for r2
 into Counter + (4 + 5) % IDX_MOD.
 */
-int inst_sti(champion_t *, core_t *, code_t, int *);
+int inst_sti();
 
 /* 
 Takes 1 parameter, an index. Creates a new program, inheriting states
 from the parent, and starting execution at COunter + parameter %
 IDX_MOD
 */
-int inst_fork(champion_t *, core_t *, code_t, int *);
+int inst_fork();
 
 /* 
 Same as ld without the %IDX_MOD. Modifies the carry
 */
-int inst_lld(champion_t *, core_t *, code_t, int *);
+int inst_lld();
 
 /* 
 Same as ldi without the %IDX_MOD. Modifies the carry
 */
-int inst_lldi(champion_t *, core_t *, code_t, int *);
+int inst_lldi();
 
 /* 
 Same as fork without the %IDX_MOD.
 */
-int inst_lfork(champion_t *, core_t *, code_t, int *);
+int inst_lfork();
 
 /* 
 Takes 1 parameter, a register. Displays to stdout the character
 corresponding to the ASCII code of the content of the register (in base
 10). A 256 modulo is applied to this ASCII code
 */
-int inst_aff(champion_t *, core_t *, code_t, int *);
+int inst_aff();
 
 #endif
