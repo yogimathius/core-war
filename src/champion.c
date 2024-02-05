f7c40d1c1e0282ff3734d17b3a341eb372ae5d33
Mathius <info@yogimathius.dev>
2024-02-04 22:06:21 -0800

    refactor: add parse_header fn

--- src/champion.c
+++ src/champion.c
@@ -15,6 +15,39 @@ champion_t *init_champion() {
 	return champ;
 }
 
+int parse_header(champion_t *champion, int bytes_read, char *hex_buffer) {
+  int i;
+  unsigned int magic_value = 0;
+  for (i = 0; i < bytes_read && i < 4; i++) {
+      magic_value = (magic_value << 8) | (unsigned char)hex_buffer[i];
+  }
+
+  if (magic_value == 0xea83f3) {
+      champion->header.magic = magic_value;
+      printf("Magic value equals 0xea83f3!\n");
+  } else {
+      printf("Magic value does not equal 0xea.\n");
+      return 1;
+  }
+
+  for (i = 4; i < bytes_read && i < 132; i++) {
+      champion->header.prog_name[i - 4] = hex_buffer[i];
+  }
+
+  for (i = 136; i < bytes_read && i < 140; i++) {
+      champion->header.prog_size = (champion->header.prog_size << 8) | (unsigned char)hex_buffer[i];
+  }
+
+  for (i = 140; i < 140 + COMMENT_LENGTH; i++) {
+      champion->header.comment[i - 140] = hex_buffer[i];
+  }
+
+  printf("Program name: %s\n", champion->header.prog_name);
+  printf("Program size: %d\n", champion->header.prog_size);
+  printf("Comment: %s\n", champion->header.comment);
+  return 0;
+}
+
 champion_t *create_champion(champion_t *champion, char *filename) {
   int fd = open(filename, O_RDONLY);
 
@@ -35,38 +68,22 @@ champion_t *create_champion(champion_t *champion, char *filename) {
       exit(1);
     }
 
-    unsigned int magic_value = 0;
-
-    int i;
-    for (i = 0; i < bytes_read && i < 4; i++) {
-        magic_value = (magic_value << 8) | (unsigned char)hex_buffer[i];
+    if (parse_header(champion, bytes_read, hex_buffer) == 1) {
+      printf("Error: could not parse header\n");
+      exit(1);
     }
 
-    if (magic_value == 0xea83f3) {
-        champion->header.magic = magic_value;
-        printf("Magic value equals 0xea83f3!\n");
-    } else {
-        printf("Magic value does not equal 0xea.\n");
-        return NULL;
+    int i = 140 + COMMENT_LENGTH;
+    
+    while (i < bytes_read) {
+      if (hex_buffer[i] != 0) {
+        // converting to hex
+        printf("hex item at %d: %0x\n", i, hex_buffer[i]);
+
+      }
+      i++;
     }
-
-    for (i = 4; i < bytes_read && i < 132; i++) {
-        champion->header.prog_name[i - 4] = hex_buffer[i];
-    }
-
-    for (i = 136; i < bytes_read && i < 140; i++) {
-        champion->header.prog_size = (champion->header.prog_size << 8) | (unsigned char)hex_buffer[i];
-    }
-
-    for (i = 140; i < bytes_read && i < 2140; i++) {
-        champion->header.comment[i - 140] = hex_buffer[i];
-    }
-
-    printf("Program name: %s\n", champion->header.prog_name);
-    printf("Program size: %d\n", champion->header.prog_size);
-    printf("Comment: %s\n", champion->header.comment);
-
-
+    
   } else {
       perror("Error getting file size");
   }
