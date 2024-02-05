b47f20f3d031cf71d52bb2683bba26e1c091437f
Prakash <prakashroj@gmail.com>
2024-02-04 22:50:14 -0500

    test_add_champion implemented

--- tests/champion_test.c
+++ tests/champion_test.c
@@ -14,6 +14,25 @@ void test_init_champion() {
     assert_true(champ->id == 1);
 }
 
+void test_add_champion(void **state){
+    //Initialize VM and champion
+    core_t core_vm;
+    init_vm(&core_vm);
+    champion_t *champ = init_champion(1);
+
+    // call add_champion with vm and champ as args
+    int result = add_champion(&core_vm, champ);
+
+    // assert vm.champion_count == 1
+    assert_int_equal(core_vm.champion_count, 1);
+
+    // assert first champion in vm list of champions is just initialized champion
+    assert_true(core_vm.champions[0] == champ);
+
+    //cleanup
+    free(champ);
+}
+
 // void test_load_champion() {
 
 // }
--- tests/main_test.c
+++ tests/main_test.c
@@ -29,6 +29,7 @@ int main(void) {
         cmocka_unit_test(test_read_memory),
         cmocka_unit_test(test_write_memory),
         cmocka_unit_test(test_init_champion),
+        cmocka_unit_test(test_add_champion),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
