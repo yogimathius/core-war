59aeb0bbf3a650287d8f0fe42c8c3ceb0c2d0a4e
Mathius <info@yogimathius.dev>
2024-02-06 01:41:40 -0800

    fix: add args to vm tests

--- tests/vm_test.c
+++ tests/vm_test.c
@@ -8,7 +8,8 @@
 
 core_t core_vm;
 
-void test_initialize_memory() {
+void test_initialize_memory(void **state) {
+    UNUSED(state);
 
     init_vm(&core_vm);
     assert_true(&core_vm != NULL);
@@ -17,12 +18,14 @@ void test_initialize_memory() {
     assert_true(core_vm.champion_count == 0);
 }
 
-void test_read_memory() {
+void test_read_memory(void **state) {
+    UNUSED(state);
     core_vm.registers[0] = 42;
     assert_true(read_vm(&core_vm, 0) == 42);
 }
 
-void test_write_memory() {
+void test_write_memory(void **state) {
+    UNUSED(state);
     write_vm(&core_vm, 1, 24);
     assert_true(read_vm(&core_vm, 1) == 24);
 }
