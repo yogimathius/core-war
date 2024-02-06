462d1cd048a37b90e7082210f0a3570d4e5c6773
Mathius <info@yogimathius.dev>
2024-02-06 01:16:42 -0800

    chore: small edits to simple_2.s

--- players/simple_2.s
+++ players/simple_2.s
@@ -2,10 +2,12 @@
 .comment "Let's get started"
 
 l2:		   sti  [Par. Des.]   r1  %:live    %1
-        ;  0b      68         01   00 0f   00 01     ; Hex. representation
+        ;  0b      68                 01    0f      01
+        ; Hex. representation
           
          and  [Par. Des.]    r1      %0        r1     
-        ;  06       64         01  00 00 00 00   01    ; Hex. representation
+        ;  06       64       01      00        01
+        ; Hex. representation
 
 live:	live %1
 		zjmp %:live
