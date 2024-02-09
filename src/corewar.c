6fb087e771d51e8cf2c8f6a7447cb917923a6eb1
Mathius <info@yogimathius.dev>
2024-02-09 00:10:35 -0800

    chore: use memset to initialize core memory and registers

--- src/corewar.c
+++ src/corewar.c
@@ -10,8 +10,7 @@
 
 int main(int ac, char **av) {
     printf("Starting corewar\n");
-    core_t vm;
-    vm.instruction_pointer = 0;
+    core_t *core_vm = init_vm();
     if (ac < 2) {
         printf("Usage: ./corewar [champion1.cor] [champion2.cor] [champion3.cor] [champion4.cor]\n");
         exit(1);
@@ -20,14 +19,14 @@ int main(int ac, char **av) {
         printf("Loading champion: %s\n", av[ac - 1]);
         champion_t *champ = init_champion();
         create_champion(champ, av[ac - 1]);
-        add_champion(&vm, champ);
+        add_champion(core_vm, champ);
         ac--;
     }
 
     int i = 0;
-    while (i < vm.champion_count) {
-        printf("Champion P%d: %s\n", vm.champions[i].id, vm.champions[i].header.prog_name);
-        run_champion(&vm, vm.champions[i]);
+    while (i < core_vm->champion_count) {
+        printf("Champion P%d: %s\n", core_vm->champions[i].id, core_vm->champions[i].header.prog_name);
+        run_champion(core_vm, core_vm->champions[i]);
         i++;
     }
 
