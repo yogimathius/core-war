7d212f9c35c9dd31767a8bb6926915881caf7e87
Mathius <info@yogimathius.dev>
2024-02-10 13:28:25 -0800

    chore: update logging

--- src/corewar.c
+++ src/corewar.c
@@ -15,6 +15,8 @@ int main(int ac, char **av) {
         printf("Usage: ./corewar [champion1.cor] [champion2.cor] [champion3.cor] [champion4.cor]\n");
         exit(1);
     }
+    printf("====================LOADING CHAMPIONS=====================\n");
+
     while (ac > 1) {
         printf("Loading champion: %s\n", av[ac - 1]);
         champion_t *champ = init_champion();
@@ -24,6 +26,8 @@ int main(int ac, char **av) {
     }
 
     int i = 0;
+    printf("\n\n");
+    printf("====================START GAME=====================\n");
     while (i < core_vm->champion_count) {
         printf("Champion P%d: %s\n", core_vm->champions[i].id, core_vm->champions[i].header.prog_name);
         run_champion(core_vm, core_vm->champions[i]);
