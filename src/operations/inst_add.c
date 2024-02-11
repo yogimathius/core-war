065f71432b20f8e4bcd180bccbf80e3db0d6f18b
Mathius <info@yogimathius.dev>
2024-02-10 22:34:29 -0800

    feat: complete barebones inst_add with tests

--- src/operations/inst_add.c
+++ src/operations/inst_add.c
@@ -8,7 +8,21 @@ Takes 3 parameters: 3 registers. Add the first to the second, and store
 the result to the third. Modifies the carry.
 */
 int inst_add(champion_t *champion, core_t *core, code_t code, int *inst) {
-  log_instruction_args(champion, core, code, inst);
+    log_instruction_args(champion, core, code, inst);
 
-  return 0;
+    int operand1 = champion->registers[inst[0]];
+    int operand2 = champion->registers[inst[1]];
+    int destination_register_index = inst[2];
+
+    int result = operand1 + operand2;
+
+    champion->registers[destination_register_index] = result;
+
+    if (result == 0) {
+        champion->carry_flag = 1;
+    } else {
+        champion->carry_flag = 0;
+    }
+
+    return 0;
 };
