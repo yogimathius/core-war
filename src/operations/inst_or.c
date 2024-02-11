d46684152aa994e55e38a3773713021186960cd4
Mathius <info@yogimathius.dev>
2024-02-10 22:55:21 -0800

    feat: complete barebones inst_or with tests

--- src/operations/inst_or.c
+++ src/operations/inst_or.c
@@ -1,13 +1,25 @@
-#include "../../include/op.h"
-#include "../../include/instructions.h"
-
-#include <stdio.h>
+#include <op.h>
+#include <instructions.h>
 
 /* 
 Same as and, but performing an OR.
 */
 int inst_or(champion_t *champion, core_t *core, code_t code, int *inst) {
   log_instruction_args(champion, core, code, inst);
+
+  int operand1 = champion->registers[inst[0]];
+  int operand2 = champion->registers[inst[1]];
+  int destination_register_index = inst[2];
+
+  int result = operand1 | operand2;
+
+  champion->registers[destination_register_index] = result;
+
+  if (result == 0) {
+    champion->carry_flag = 1;
+  } else {
+    champion->carry_flag = 0;
+  }
 
   return 0;
 };
--- tests/operations/inst_or_test.c
+++ tests/operations/inst_or_test.c
@@ -7,9 +7,34 @@
 
 void inst_or_test(void **state) {
 	UNUSED(state);
-	champion_t *champion = NULL;
-	core_t *core = NULL;
-	code_t code = 1;
-	int inst = 1;
-	assert_true(inst_or(champion, core, code, &inst) == 0);
+	champion_t *champ = init_champion();
+	create_champion(champ, "players/simple_2.cor");	
+	// Set some registers
+	champ->registers[1] = 1;
+	champ->registers[2] = 2;
+	core_t *core = init_vm();
+	code_t code = 7;
+	int args[] = {1, 2, 3};
+
+	assert_true(champ->registers[3] == 0);
+	assert_true(inst_or(champ, core, code, args) == 0);
+	assert_true(champ->registers[3] == 3);
+	assert_true(champ->carry_flag == 0);
+}
+
+void inst_or_test_zero(void **state) {
+	UNUSED(state);
+	champion_t *champ = init_champion();
+	create_champion(champ, "players/simple_2.cor");	
+	// Set some registers
+	champ->registers[1] = 0;
+	champ->registers[2] = 0;
+	core_t *core = init_vm();
+	code_t code = 7;
+	int args[] = {1, 2, 3};
+
+	assert_true(champ->registers[3] == 0);
+	assert_true(inst_or(champ, core, code, args) == 0);
+	assert_true(champ->registers[3] == 0);
+	assert_true(champ->carry_flag == 1);
 }
