60a35a0487b79a569f0784fc293d141d5769d4b9
Mathius <info@yogimathius.dev>
2024-02-10 23:56:13 -0800

    chore: code cleanup in instructions

--- src/operations/inst_fork.c
+++ src/operations/inst_fork.c
@@ -1,12 +1,9 @@
-#include "../../include/op.h"
-#include "../../include/instructions.h"
-
-#include <stdio.h>
+#include <op.h>
+#include <instructions.h>
 
 /* 
 Takes 1 parameter, an index. Creates a new program, inheriting states
-from the parent, and starting execution at COunter + parameter %
-IDX_MOD
+from the parent, and starting execution at Counter + parameter % IDX_MOD
 */
 int inst_fork(champion_t *champion, core_t *core, code_t code, int *inst) {
   log_instruction_args(champion, core, code, inst);
--- tests/operations/inst_ldi_test.c
+++ tests/operations/inst_ldi_test.c
@@ -1,9 +1,4 @@
-#include <stdarg.h>
-#include <stddef.h>
-#include <setjmp.h>
-#include <cmocka.h>
-#include "../../include/op.h"
-#include "../../include/tests.h"
+#include <tests.h>
 
 void inst_ldi_test(void **state) {
 	UNUSED(state);
