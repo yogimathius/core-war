b813dac747e89092009529b15c560ee79030239d
Mathius <info@yogimathius.dev>
2024-02-10 23:35:16 -0800

    feat: complete barebones inst_sti with tests

--- src/operations/inst_sti.c
+++ src/operations/inst_sti.c
@@ -1,6 +1,5 @@
-#include "../../include/op.h"
-#include "../../include/instructions.h"
-#include <stdio.h>
+#include <op.h>
+#include <instructions.h>
 
 /* 
 Takes 3 parameters. The first one is a register. The other two can either
@@ -9,5 +8,20 @@ into Counter + (4 + 5) % IDX_MOD.
 */
 int inst_sti(champion_t *champion, core_t *core, code_t code, int *inst) {
   log_instruction_args(champion, core, code, inst);
+
+  int destination_register_index = inst[0];
+  int operand1 = inst[1];
+  int operand2 = inst[2];
+
+  int result = champion->counter + (operand1 + operand2) % IDX_MOD;
+
+  champion->registers[destination_register_index] = result;
+
+  if (result == 0) {
+      champion->carry_flag = 1;
+  } else {
+      champion->carry_flag = 0;
+  }
+
   return 0;
 };
--- tests/operations/inst_sti_test.c
+++ tests/operations/inst_sti_test.c
@@ -1,15 +1,35 @@
-#include <stdarg.h>
-#include <stddef.h>
-#include <setjmp.h>
-#include <cmocka.h>
-#include "../../include/op.h"
-#include "../../include/tests.h"
+#include <tests.h>
 
 void inst_sti_test(void **state) {
 	UNUSED(state);
-	champion_t *champion = NULL;
-	core_t *core = NULL;
-	code_t code = 1;
-	int inst = 1;
-	assert_true(inst_sti(champion, core, code, &inst) == 0);
+	champion_t *champ = init_champion();
+	create_champion(champ, "players/simple_2.cor");	
+	// Set some registers
+	champ->registers[1] = 1;
+	champ->registers[2] = 2;
+	core_t *core = init_vm();
+	code_t code = OP_STI;
+	int args[] = {1, 2, 3};
+	champ->counter = 0;
+	assert_true(champ->registers[1] == 1);
+	assert_true(inst_sti(champ, core, code, args) == 0);
+	assert_true((int)champ->registers[1] == champ->counter + (2 + 3) % IDX_MOD);
+	assert_true(champ->carry_flag == 0);
+}
+
+void inst_sti_test_zero(void **state) {
+	UNUSED(state);
+	champion_t *champ = init_champion();
+	create_champion(champ, "players/simple_2.cor");	
+	// Set some registers
+	champ->registers[1] = 0;
+	champ->registers[2] = 0;
+	core_t *core = init_vm();
+	code_t code = OP_STI;
+	int args[] = {1, 2, 3};
+	champ->counter = 0;
+	assert_true(champ->registers[1] == 0);
+	assert_true(inst_sti(champ, core, code, args) == 0);
+	assert_true((int)champ->registers[1] == champ->counter + (2 + 3) % IDX_MOD);
+	assert_true(champ->carry_flag == 0);
 }
