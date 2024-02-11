4880d48e47f9c72bb7b1dab0970cfa2f199909a6
Mathius <info@yogimathius.dev>
2024-02-10 23:56:27 -0800

    feat: run new instruction tests

--- tests/main_test.c
+++ tests/main_test.c
@@ -1,25 +1,28 @@
-#include <stdarg.h>
-#include <stddef.h>
-#include <setjmp.h>
-#include <cmocka.h>
-
-#include "../include/tests.h"
+#include <tests.h>
 
 int main(void) {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test(inst_live_test),
         cmocka_unit_test(inst_ld_test),
+        cmocka_unit_test(inst_ld_test_zero),
         cmocka_unit_test(inst_st_test),
         cmocka_unit_test(inst_add_test),
+        cmocka_unit_test(inst_add_test_zero),
         cmocka_unit_test(inst_sub_test),
+        cmocka_unit_test(inst_sub_test_zero),
         cmocka_unit_test(inst_and_test),
+        cmocka_unit_test(inst_and_test_zero),
         cmocka_unit_test(inst_or_test),
+        cmocka_unit_test(inst_or_test_zero),
         cmocka_unit_test(inst_xor_test),
+        cmocka_unit_test(inst_xor_test_zero),
         cmocka_unit_test(inst_zjmp_test),
         cmocka_unit_test(inst_ldi_test),
         cmocka_unit_test(inst_sti_test),
+        cmocka_unit_test(inst_sti_test_zero),
         cmocka_unit_test(inst_fork_test),
         cmocka_unit_test(inst_lld_test),
+        cmocka_unit_test(inst_lld_test_zero),
         cmocka_unit_test(inst_lldi_test),
         cmocka_unit_test(inst_lfork_test),
         cmocka_unit_test(inst_aff_test),
