0e05d740f5825aed9d5fc48100dbdaa51dd46234
Mathius <info@yogimathius.dev>
2024-02-20 22:41:14 -0800

    chore: update players corfiles

---
+++ players/simple_3.s
@@ -0,0 +1,13 @@
+.name "Simple"
+.comment "Let's get started"
+
+l2:     sti  r1  %:live    %1
+        ;  0b      68                 01    0f      01
+        ; Hex. representation
+          
+         and   r1      %0        r1     
+        ;  06       64       01      00        01
+        ; Hex. representation
+
+live:	live %1
+		zjmp %:live
