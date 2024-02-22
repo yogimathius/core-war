58d490975dabb47626ac740dc9704464cff22821
Mathius <info@yogimathius.dev>
2024-02-22 00:03:53 -0800

    chore: cleanup logs

--- src/operations/utils.c
+++ src/operations/utils.c
@@ -3,11 +3,10 @@
 
 void log_instruction_args(const champion_t *champion, const core_t *core, code_t code, int *inst) {
   const op_t *operation = &op_tab[code];
-
+  UNUSED(champion);
+  UNUSED(core);
+  
   printf("====================called %s with:=====================\n", operation->mnemonique);
-  printf("champion: %p\n", &champion);
-  printf("core: %p\n", &core);
-  printf("code: %p\n", &code);
   int i = 0;
   printf("args in execute instructions: ");
   while (i < operation->nbr_args) {
