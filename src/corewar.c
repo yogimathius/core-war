f83923fcb75a1088ed7436c7de22c30fee62b46c
Mathius <info@yogimathius.dev>
2024-02-22 16:58:13 -0800

    chore: move init_vm to after ac guard

--- src/corewar.c
+++ src/corewar.c
@@ -5,11 +5,11 @@
 
 int main(int ac, char **av) {
     printf("Starting corewar\n");
-    core_t *core_vm = init_vm();
     if (ac < 2) {
         printf("Usage: ./corewar [champion1.cor] [champion2.cor] [champion3.cor] [champion4.cor]\n");
         exit(1);
     }
+    core_t *core_vm = init_vm();
     
     load_champions(core_vm, ac, av);
 
