bfa5113f0135c2e680604a94b8dc1d8ac5e331d0
Mathius <info@yogimathius.dev>
2024-02-24 11:55:59 -0800

    chore: small update to inst_live logs

--- src/operations/inst_live.c
+++ src/operations/inst_live.c
@@ -9,7 +9,7 @@ int inst_live(champion_t *champion, core_t *core, code_t code, int *inst) {
   log_instruction_args(champion, core, code, inst);
   core->lives++;
   core->winner = champion->id;
-  
-  printf("The program P%d(%s) is alive\n", champion->id, champion->header.prog_name);
+
+  printf("The program P%d(%s) is alive\n\n", champion->id, champion->header.prog_name);
   return 0;
 };
