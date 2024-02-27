1021fbe0aaa1a98f576cd9ab403bcbc4d6ae8351
Mathius <info@yogimathius.dev>
2024-02-26 23:58:08 -0800

    chore: add colored text to inst_live

--- src/operations/inst_live.c
+++ src/operations/inst_live.c
@@ -10,6 +10,8 @@ int inst_live(champion_t *champion, core_t *core, code_t code, int *inst) {
   core->lives++;
   core->winner = champion->id;
 
+  print_colored_text(champion->color);
   printf("The program P%d(%s) is alive\n\n", champion->id, champion->header.prog_name);
+  printf("\033[0m");
   return 0;
 };
