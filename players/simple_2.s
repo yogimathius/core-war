0a170f211a232238714ff172f8f54752b25268c6
Mathius <info@yogimathius.dev>
2024-02-27 23:50:19 -0800

    chore: update comments in simple_2.s

--- players/simple_2.s
+++ players/simple_2.s
@@ -1,13 +1,11 @@
 .name "Simple"
 .comment "Let's get started"
 
-l2:		   sti  [Par. Des.]   r1  %:live    %1
-        ;  0b      68                 01    0f      01
-        ; Hex. representation
+l2:	sti   r1  %:live %1
+        ; 0b 68 01 0f 01
           
-         and  [Par. Des.]    r1      %0        r1     
-        ;  06       64       01      00        01
-        ; Hex. representation
+        and   r1   %0  r1     
+        ; 06 64 01 00 01
 
 live:	live %1
-		zjmp %:live
+                zjmp %:live
