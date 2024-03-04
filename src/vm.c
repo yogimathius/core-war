940b9c463e4a32a481882dea399842059f7e0ab5
Mathius <info@yogimathius.dev>
2024-03-03 23:02:54 -0800

    cleanup: remove load instructions in add_champion fn

--- src/vm.c
+++ src/vm.c
@@ -58,11 +58,6 @@ void add_champion(core_t *core_t, champion_t *champion) {
   champion->registers[0] = champion->id;
   champion->color = champ_color[champion->id - 1];
   core_t->champions[core_t->champion_count - 1] = *champion;
-  for (int i = 0; i < champion->instruction_size; i++) {
-    printf("\n\nAdding instruction: %d\n", champion->inst[i].opcode);
-    printf("to memory: %d\n\n", i + (MEM_SIZE / core_t->champion_count) * (core_t->champion_count - 1));
-    core_t->memory[i + (MEM_SIZE / core_t->champion_count) * (core_t->champion_count - 1)] = champion->inst[i].opcode;
-  }
 }
 
 void load_instructions(core_t *core) {
