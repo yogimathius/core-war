ecaf844cc2e1a85ab273e3362c1c22bf64c31ec3
Mathius <info@yogimathius.dev>
2024-03-04 00:43:34 -0800

    chore: cleanup includes in utils and instructions

--- src/operations/utils.c
+++ src/operations/utils.c
@@ -1,13 +1,32 @@
-#include "../../include/op.h"
+#include <op.h>
 
 void print_colored_text(int color) { printf("\033[1;%dm", color); }
 
+void print_champion_registers(const champion_t *champion) {
+  print_colored_text(champion->color);
+  printf("Champion P%d registers: ", champion->id);
+  for (int i = 0; i < REG_NUMBER; i++) {
+    printf("%d ", champion->registers[i]);
+  }
+  printf("\n");
+  printf("\033[0m");
+}
+
+void print_champion_lives(const champion_t *champion) {
+  print_colored_text(champion->color);
+  printf("Champion P%d lives: %d\n", champion->id, champion->lives);
+  printf("\033[0m");
+}
+
+void display_champion_status(const champion_t *champion) {
+  print_champion_registers(champion);
+  print_champion_lives(champion);
+}
+
 void log_instruction_args(const champion_t *champion, const core_t *core, code_t code, int *inst) {
   const op_t *operation = &op_tab[code];
-  UNUSED(champion);
-  UNUSED(core);
   print_colored_text(champion->color);
-  printf("Champion P%d: %s called %s with: ", champion->id,champion->header.prog_name, operation->mnemonique);
+  printf("Champion P%d called %s with: ", champion->id, operation->mnemonique);
   int i = 0;
   while (i < operation->nbr_args) {
     if (inst[i]) {
@@ -17,6 +36,7 @@ void log_instruction_args(const champion_t *champion, const core_t *core, code_t
   }
   printf("\n");
 
+  display_champion_status(champion);
   print_colored_text(champion->color);
   printf("Cycle: %d ", core->nbr_cycles);
   printf("Cycle to die: %d ", core->cycle_to_die);
@@ -27,3 +47,6 @@ void log_instruction_args(const champion_t *champion, const core_t *core, code_t
 int load_value_from_address(core_t *core, int address) {
     return core->memory[address % MEM_SIZE]; 
 }
+
+
+
