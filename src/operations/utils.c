9d37fde6cd305cc89b8b569514ee16ddbc808405
Mathius <info@yogimathius.dev>
2024-03-04 00:24:27 -0800

    feat: add logs for cycles in log_instruction_args

--- src/operations/utils.c
+++ src/operations/utils.c
@@ -10,12 +10,18 @@ void log_instruction_args(const champion_t *champion, const core_t *core, code_t
   printf("Champion P%d: %s called %s with: ", champion->id,champion->header.prog_name, operation->mnemonique);
   int i = 0;
   while (i < operation->nbr_args) {
-    printf("%d ", inst[i]);
+    if (inst[i]) {
+      printf("%d ", inst[i]);
+    }
     i++;
   }
-  printf("\033[0m");
+  printf("\n");
 
-  printf("\n\n");
+  print_colored_text(champion->color);
+  printf("Cycle: %d ", core->nbr_cycles);
+  printf("Cycle to die: %d ", core->cycle_to_die);
+  printf("Core Lives: %d\n\n", core->lives);
+  printf("\033[0m");
 }
 
 int load_value_from_address(core_t *core, int address) {
