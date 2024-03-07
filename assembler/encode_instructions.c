9e64ba5d75649ee436e3cd580dabbf21ed9592cc
Mathius <info@yogimathius.dev>
2024-03-07 00:14:26 -0800

    wip: fix direct and indirect encoding

--- assembler/encode_instructions.c
+++ assembler/encode_instructions.c
@@ -3,16 +3,16 @@
 // Ensure that registers are encoded as a single byte
 void encode_register(FILE *output, const char *arg) {
     unsigned char reg_num = (unsigned char)atoi(arg + 1); // Skip 'r' and convert to int
-    fwrite(&reg_num, T_REG, 1, output); // Write a single byte
+    fwrite(&reg_num, REG_SIZE, 1, output); // Write a single byte
 }
 
 
-void encode_direct(FILE *output, const char *arg) {
+void encode_indirect(FILE *output, const char *arg) {
     int direct_value = atoi(arg);
-
+    printf("direct_value: %d\n", direct_value);
     unsigned int big_endian_value = htonl(direct_value);
-
-    fwrite(&big_endian_value, T_DIR, 1, output);
+    printf("big_endian_value: %u\n", big_endian_value);
+    fwrite(&big_endian_value, IND_SIZE, 1, output);
 }
 
 int containsDigits(const char *str) {
@@ -25,23 +25,25 @@ int containsDigits(const char *str) {
     return 0;
 }
 
-void encode_indirect(FILE *output, const char *arg, int current_address) {
+void encode_direct(FILE *output, const char *arg, int current_address) {
     int indirect_value;
     if (arg[0] == '%' && containsDigits(arg + 1) == 0) {
+        printf("label: %s\n", arg);
         // Handle as label
-        int label_address = lookup_symbol(arg + 1); // Skip '%' and lookup label
+        int label_address = lookup_symbol(arg + 2); // Skip '%' and lookup label
+        printf("label_address: %d\n", label_address);
         indirect_value = label_address - current_address;
     } else if (arg[0] == '%' && containsDigits(arg + 1) == 1) {
         indirect_value = atoi(arg + 1); // Skip '%' and convert to int
     } else {
-        printf("arg in indirect: %s\n", arg);
+        printf("arg in direct: %s\n", arg);
         // Handle as numeric offset
         indirect_value = atoi(arg);
     }
     
     unsigned int big_endian_value = htonl(indirect_value);
 
-    fwrite(&big_endian_value, T_IND, 1, output);
+    fwrite(&big_endian_value, DIR_SIZE, 1, output);
 }
 
 
@@ -88,7 +90,6 @@ void encode_instruction(FILE *output, parsed_line_t *parsedLine) {
     // printf("\n===================ENCODING PD==================\n");
     // Then write the parameter description byte
     unsigned char param_description = encode_parameter_description(parsedLine->arguments, parsedLine->argumentCount);
-    printf("param_description: %d\n", param_description);
     fwrite(&param_description, sizeof(param_description), 1, output);
 
     // Then write the arguments
@@ -100,18 +101,21 @@ void encode_instruction(FILE *output, parsed_line_t *parsedLine) {
         while (isspace((unsigned char)*arg)) { arg++; }
         printf("arg in encode: %s\n", arg);
         if (*arg == 'r') {
-            // Write register (1 byte)
             printf("register: %s\n", arg);
             encode_register(output, arg);
         } else if (*arg == '%') {
-            // Write indirect (2 bytes)
-            printf("indirect: %s\n", arg);
+            printf("direct: %s\n", arg);
             // (*arg)++;
-            encode_indirect(output, arg, 0); // Replace 0 with the current address
+            // if (*arg == LABEL_CHAR) {
+            //     printf("label: %s\n", arg);
+            //     (*arg)++;
+            // }
+            
+            encode_direct(output, arg, 0); 
         } else if (isdigit(*arg) || (*arg == '-' && isdigit(*(arg + 1)))) {
             // Write direct (4 bytes)
-            printf("direct: %s\n", arg);
-            encode_direct(output, arg);
+            printf("indirect: %s\n", arg);
+            encode_indirect(output, arg);
         } else {
             // Handle error accordingly, but do not exit.
             fprintf(stderr, "Error: Invalid argument '%s'.\n", arg);
