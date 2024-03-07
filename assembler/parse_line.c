51d82725632182ae0d64bd6dbb5a5a436543dd45
Mathius <info@yogimathius.dev>
2024-03-07 00:15:56 -0800

    fix: add token label when label found

--- assembler/parse_line.c
+++ assembler/parse_line.c
@@ -3,18 +3,19 @@
 parsed_line_t parse_line(const char *line) {
     parsed_line_t parsedLine = {TOKEN_UNKNOWN, "", "", {""}, 0};
     const char *inputPtr = line;
-
+    printf("inputPtr: %s\n", inputPtr);
     Token token;
 
     // Check for empty line or comment
     token = lex_token(&inputPtr);
-    if (token.type == TOKEN_ENDLINE || token.type == TOKEN_COMMENT) {
+    if (token.type == TOKEN_BLANKLINE || token.type == TOKEN_COMMENT) {
         parsedLine.lineType = token.type;
         return parsedLine;
     }
 
     // Process the first token which could be a label or an instruction
     if (token.type == TOKEN_LABEL) {
+        parsedLine.lineType = TOKEN_LABEL;
         strcpy(parsedLine.label, token.string);
         token = lex_token(&inputPtr); // Get next token after label
     }
@@ -29,7 +30,7 @@ parsed_line_t parse_line(const char *line) {
 
     // Process arguments if any
     // printf("\n=======parsing arguments======\n");
-    while ((token = lex_token(&inputPtr)).type != TOKEN_ENDLINE && token.type != TOKEN_COMMENT) {
+    while ((token = lex_token(&inputPtr)).type != TOKEN_BLANKLINE && token.type != TOKEN_COMMENT) {
         if (parsedLine.argumentCount < MAX_ARGS_NUMBER && token.string[0] != '\0') {
             // printf("found arg: %s\n", token.string);
             strcpy(parsedLine.arguments[parsedLine.argumentCount], token.string);
