1df1ab5be5326a873bc9114efe1854c315248192
Mathius <info@yogimathius.dev>
2024-03-07 00:15:32 -0800

    chore: stop parse_header after name and comment found

--- assembler/parse_header.c
+++ assembler/parse_header.c
@@ -1,19 +1,19 @@
 #include "./op.h"
 
 FileHeader parse_header(FILE *input) {
-    FileHeader header = {"", ""};
+    FileHeader header = {"", "", 0};
     char line[MAX_LINE_LENGTH];
-
-    while (fgets(line, sizeof(line), input)) {
+    int name_found = 0;
+    int comment_found = 0;
+    while (fgets(line, sizeof(line), input) && (!name_found || !comment_found)) {
         if (strncmp(line, ".name", 5) == 0) {
             sscanf(line, ".name \"%[^\"]\"", header.name);
+            name_found = 1;
         } else if (strncmp(line, ".comment", 8) == 0) {
             sscanf(line, ".comment \"%[^\"]\"", header.comment);
-        } else {
-            break;
+            comment_found = 1;
         }
     }
-
     rewind(input);
 
     return header;
