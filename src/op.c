ff29813d7e0e81262f85cbdeefabf5ec01c07dff
Mathius <info@yogimathius.dev>
2024-03-08 17:24:09 -0800

    chore: convert one arg instructions type to IND

--- src/op.c
+++ src/op.c
@@ -1,7 +1,7 @@
 #include "../include/op.h"
 
 const op_t op_tab[] = {
-  {"live", 1, {T_DIR}, 1, 10, inst_live},
+  {"live", 1, {T_IND}, 1, 10, inst_live},
   {"ld", 2, {T_DIR | T_IND, T_REG}, 2, 5, inst_ld},
   {"st", 2, {T_REG, T_IND | T_REG}, 3, 5, inst_st},
   {"add", 3, {T_REG, T_REG, T_REG}, 4, 10, inst_add},
@@ -15,11 +15,11 @@ const op_t op_tab[] = {
   {"zjmp", 1, {T_DIR}, 9, 20, inst_zjmp},
   {"ldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 10, 25, inst_ldi},
   {"sti", 3, {T_REG, T_REG | T_DIR | T_IND, T_DIR | T_REG}, 11, 25, inst_sti},
-  {"fork", 1, {T_DIR}, 12, 800, inst_fork},
+  {"fork", 1, {T_IND}, 12, 800, inst_fork},
   {"lld", 2, {T_DIR | T_IND, T_REG}, 13, 10, inst_lld},
   {"lldi", 3, {T_REG | T_DIR | T_IND, T_DIR | T_REG, T_REG}, 14, 50,
   inst_lldi},
-  {"lfork", 1, {T_DIR}, 15, 1000, inst_lfork},
+  {"lfork", 1, {T_IND}, 15, 1000, inst_lfork},
   {"aff", 1, {T_REG}, 16, 2, inst_aff},
   {0,0,{0},0,0,0}
 };
